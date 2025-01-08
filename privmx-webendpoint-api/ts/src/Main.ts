dd8ce99bcf6a6793120824887282a8f5a4b016cb
Kamil Zuranski <zuranski83@gmail.com>
2025-01-08 13:44:25 +0100

    feat: bundle and import in one build

---
+++ privmx-webendpoint-api/ts/src/Main.ts
@@ -0,0 +1,29 @@
+/*!
+PrivMX Web Endpoint.
+Copyright © 2024 Simplito sp. z o.o.
+
+This file is part of the PrivMX Platform (https://privmx.dev).
+This software is Licensed under the PrivMX Free License.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { EndpointFactory } from "./service/EndpointFactory";
+
+declare function endpointWasmModule(): Promise<any>; // Provided by emscripten js glue code
+
+function setLib(){
+    endpointWasmModule().then((lib: any)=>{
+        EndpointFactory.init(lib);
+        dispatchEvent(new CustomEvent('libInitialized'));
+    }).catch((err: Error)=>console.log(err));
+}
+
+addEventListener("wasmLoaded", setLib);
+
+export {EndpointFactory}
+
+/**
+ * TODO: komunikat o niepowodzeniu ladowania wasma - np ze prawdopodobnie brakuje plików w public
+ */
---
+++ privmx-webendpoint-api/ts/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+  entry: {
+    bundle: './src/bundle.ts'
+  },
+  mode: 'production',
+  plugins: [
+    new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+        "crypto": require.resolve('crypto-browserify'), 
+        "assert": require.resolve('assert/'),
+        "stream": require.resolve("stream-browserify"),
+        "buffer": require.resolve("buffer"),
+        "vm": require.resolve("vm-browserify"),
+        'process/browser': require.resolve('process/browser'),
+        
+      } 
+  },
+  output: {
+    filename: (pathData) => {
+      return pathData.chunk.name == 'bundle' ? 'privmx-endpoint-web.js' : 'privmx-endpoint-web.[name].js';
+    },
+    path: path.resolve(__dirname, 'dist/bundle'),
+  },
+};
