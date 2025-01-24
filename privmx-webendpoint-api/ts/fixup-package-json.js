f59eac075b293e65c86e19761bf5df812d6584b2
Kamil Zuranski <zuranski83@gmail.com>
2025-01-24 14:34:08 +0100

    fix: expose extras right after package name

--- privmx-webendpoint-api/ts/fixup-package-json.js
+++ privmx-webendpoint-api/ts/fixup-package-json.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const DIST_BUNDLE_PATH = "dist/bundle";
-const DIST_MJS_PATH = "dist";
+const DIST_BUNDLE_PATH = "bundle";
+// const DIST_MJS_PATH = "dist";
 
 if (fs.existsSync(DIST_BUNDLE_PATH)) {
   fs.appendFileSync(path.resolve(DIST_BUNDLE_PATH, "package.json"), JSON.stringify({
@@ -10,9 +10,9 @@ if (fs.existsSync(DIST_BUNDLE_PATH)) {
   }, null, 2));
 }
 
-if (fs.existsSync(DIST_MJS_PATH)) {
-  fs.appendFileSync(path.resolve(DIST_MJS_PATH, "package.json"), JSON.stringify({
-    main: DIST_MJS_PATH + "/index.js",
-    types: DIST_MJS_PATH + "/index.d.ts"
-  }, null, 2));  
-}
+// if (fs.existsSync(DIST_MJS_PATH)) {
+//   fs.appendFileSync(path.resolve(DIST_MJS_PATH, "package.json"), JSON.stringify({
+//     main: DIST_MJS_PATH + "/index.js",
+//     types: DIST_MJS_PATH + "/index.d.ts"
+//   }, null, 2));  
+// }
