fe3a1c52a9d4f758d3a8a962c34bc88ebde2d3ea
Kamil Zuranski <zuranski83@gmail.com>
2025-01-25 01:20:51 +0100

    fix: incorrect api bindings

--- privmx-webendpoint-api/ts/src/api/CryptoApiNative.ts
+++ privmx-webendpoint-api/ts/src/api/CryptoApiNative.ts
@@ -31,8 +31,8 @@ export class CryptoApiNative extends BaseNative {
     async derivePrivateKey(ptr: number, args: [string, string]): Promise<string> {
         return this.runAsync<string>((taskId)=>this.api.lib.CryptoApi_derivePrivateKey(taskId, ptr, args));
     }
-    async derivePrivateKey_deprecated(ptr: number, args: [string, string]): Promise<string> {
-        return this.runAsync<string>((taskId)=>this.api.lib.CryptoApi_derivePrivateKey_deprecated(taskId, ptr, args));
+    async derivePrivateKey2(ptr: number, args: [string, string]): Promise<string> {
+        return this.runAsync<string>((taskId)=>this.api.lib.CryptoApi_derivePrivateKey2(taskId, ptr, args));
     }
     async derivePublicKey(ptr: number, args: [string]): Promise<string> {
         return this.runAsync<string>((taskId)=>this.api.lib.CryptoApi_derivePublicKey(taskId, ptr, args));
