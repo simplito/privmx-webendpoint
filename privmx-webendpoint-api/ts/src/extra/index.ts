0935d3b401c9b5fd46e500b6e75fa8f08592ffff
Filip Hejmowski <fhejmowski@simplito.com>
2025-02-26 13:18:01 +0100

    feat: privmx connection wrapper for easier api handling

--- privmx-webendpoint-api/ts/src/extra/index.ts
+++ privmx-webendpoint-api/ts/src/extra/index.ts
@@ -1,28 +1,40 @@
-import * as Files from "./files";
-import * as Events from "./events";
-import { EventManager, ThreadEventsManager, StoreEventsManager, InboxEventsManager, BaseEventManager, ConnectionEventsManager,  Channel, EventPayload, GenericEvent  } from "./events";
-import * as Utils from "./utils";
-import * as Generics from "./generics";
-import * as Inboxes from "./inbox";
+import * as Files from './files';
+import * as Events from './events';
+import {
+  EventManager,
+  ThreadEventsManager,
+  StoreEventsManager,
+  InboxEventsManager,
+  BaseEventManager,
+  ConnectionEventsManager,
+  Channel,
+  EventPayload,
+  GenericEvent,
+} from './events';
+import * as Utils from './utils';
+import * as Generics from './generics';
+import * as Inboxes from './inbox';
 
-import { FileUploader, StreamReader, downloadFile } from "./files";
+import { FileUploader, StreamReader, downloadFile } from './files';
+import { PrivmxClient } from './PrivmxClient';
 
 export {
-    Files, 
-    Inboxes, 
-    Events, 
-    Utils, 
-    Generics, 
-    FileUploader,
-    downloadFile,
-    EventManager, 
-    ThreadEventsManager, 
-    StoreEventsManager, 
-    InboxEventsManager, 
-    BaseEventManager, 
-    StreamReader,
-    Channel, 
-    ConnectionEventsManager,
-    EventPayload,
-    GenericEvent
+  Files,
+  Inboxes,
+  Events,
+  Utils,
+  Generics,
+  FileUploader,
+  downloadFile,
+  EventManager,
+  ThreadEventsManager,
+  StoreEventsManager,
+  InboxEventsManager,
+  BaseEventManager,
+  StreamReader,
+  PrivmxClient,
+  Channel,
+  ConnectionEventsManager,
+  EventPayload,
+  GenericEvent,
 };
