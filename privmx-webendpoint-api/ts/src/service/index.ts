0d6a91d5a9812938cb7eecddec4341ec98341291
Kamil Zuranski <zuranski83@gmail.com>
2025-07-30 16:01:25 +0200

    fix: update imports

--- privmx-webendpoint-api/ts/src/service/index.ts
+++ privmx-webendpoint-api/ts/src/service/index.ts
@@ -3,10 +3,12 @@ import { ThreadApi } from "./ThreadApi";
 import { StoreApi } from "./StoreApi";
 import { InboxApi } from "./InboxApi";
 import { KvdbApi } from "./KvdbApi";
+import { EventApi } from "./EventApi";
 import { CryptoApi } from "./CryptoApi";
+import { StreamApi } from "./StreamApi";
 import { Connection } from "./Connection";
 import { EventQueue } from "./EventQueue";
 import { BaseApi } from "./BaseApi";
 import { ExtKey } from "./ExtKey";
 
-export {EndpointFactory, ThreadApi, StoreApi, InboxApi, KvdbApi, CryptoApi, Connection, EventQueue, BaseApi, ExtKey};
+export {EndpointFactory, ThreadApi, StoreApi, InboxApi, KvdbApi, CryptoApi, EventApi, StreamApi, Connection, EventQueue, BaseApi, ExtKey};
