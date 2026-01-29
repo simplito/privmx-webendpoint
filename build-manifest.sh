043b5f093a50e6a3f3832f9aa8c9ce5f6690bc6e
Kamil Zuranski <zuranski83@gmail.com>
2026-01-29 10:58:45 +0100

    Version 2.7.0-rc1

--- build-manifest.sh
+++ build-manifest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PRIVMX_ENDPOINT="devel"
+# PRIVMX_ENDPOINT="feat/stream-room-id-on-recording"
+PRIVMX_ENDPOINT="58e23a5b7e4cd021d59da75b5be433f90e2f6332"
 PSON_CPP="v1.0.7"
 GMP="6.3.0"
 POCO="1.10.1"
